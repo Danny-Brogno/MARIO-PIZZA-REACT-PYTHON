0a9c7b5ed5fbdc719992cc499c3886de932b084f
danny <danny.brogno@gmail.com>
2026-01-23 10:20:05 +0100

    aboutUs page - second  commit

--- src/components/underHeaderAboutUs.js
+++ src/components/underHeaderAboutUs.js
@@ -1,21 +1,44 @@
 import React from 'react';
+import leaves1 from '../img/leaves1.png';
 
 export const UnderHeaderAboutUs = () => {
   
   return (
-    <div className="under-header-AboutUs">
-      <h2>ABOUT US PAGE</h2>
-      <h2>ABOUT US PAGE</h2>
-      <h2>ABOUT US PAGE</h2>
-      <h2>ABOUT US PAGE</h2>
-      <h2>ABOUT US PAGE</h2>
-      <h2>ABOUT US PAGE</h2>
-      <h2>ABOUT US PAGE</h2>
-      <h2>ABOUT US PAGE</h2>
-      <h2>ABOUT US PAGE</h2>
-      <h2>ABOUT US PAGE</h2>
-      <h2>ABOUT US PAGE</h2>
-      <h2>ABOUT US PAGE</h2>
+    <div class="about-us-container">
+    
+      <div class="set">
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+      </div>
+
+      <div class="set set2">
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+      </div>
+
+      <div class="set set3">
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+        <div><img src={leaves1} alt="" /></div>
+      </div>
+
     </div>
   );
 };
